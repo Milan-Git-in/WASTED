691735c2508eb49583865a43020db3cf615063a9
Milan-Prajapati <milanprajapati366@gmail.com>
2025-08-22 12:04:42 +0530

    add cors

--- wasted/settings.py
+++ wasted/settings.py
@@ -37,12 +37,21 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
+    "corsheaders",
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://eco-bid-mocha.vercel.app/",
+]
+
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
